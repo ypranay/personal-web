b28d447f5f1925d9a53854d2066b058d5a1c1b5b
Pranay Yadav <pranayyadav@Accels-MacBook-Pro.local>
2020-12-23 15:59:45 +0530

    some css and content changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,7 @@ function Index() {
         >
         Accel
         </a>{' '}
-        I am passionate about enterprise and open-source software, developer tools and API-first businesses.
+        . I am passionate about enterprise and open-source software, developer tools and API-first businesses.
       </p>
       <br />
       <h2>The date according to Node.js (TypeScript) is:</h2>
