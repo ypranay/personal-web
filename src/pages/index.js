079ca5d750ca697e581052c9e138a7016c3b28ad
Pranay Yadav <pranayyadav@Accels-MacBook-Pro.local>
2020-12-23 13:51:17 +0530

    fixed padding

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,40 +15,25 @@ function Index() {
   return (
     <main>
       <Helmet>
-        <title>Gatsby + Node.js (TypeScript) API</title>
+        <title>Pranay Yadav</title>
       </Helmet>
-      <h1>Gatsby + Node.js (TypeScript) API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
+      <div class = "bio-container">
+      <h1>Pranay Yadav</h1>
       <p>
+        Currently an early-stage SaaS investor at
         <a
           href="https://github.com/vercel/vercel/tree/master/examples/gatsby"
           target="_blank"
           rel="noreferrer noopener"
         >
-          This project
+        Accel::after
         </a>{' '}
-        is a <a href="https://www.gatsbyjs.org/">Gatsby</a> app with two
-        directories, <code>/src</code> for static content and <code>/api</code>{' '}
-        which contains a serverless{' '}
-        <a href="https://nodejs.org/en/">Node.js (TypeScript)</a> function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Node.js (TypeScript)
-        </a>
-        .
+        I am passionate about enterprise and open-source software, developer tools and API-first businesses.
       </p>
       <br />
       <h2>The date according to Node.js (TypeScript) is:</h2>
       <p>{date ? date : 'Loading date...'}</p>
+      </div>
     </main>
   );
 }
