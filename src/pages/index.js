4d349deb85239d2bad7433fb97e9e824ad760b60
ypranay <y.pranay@gmail.com>
2021-01-03 03:55:23 +0530

    adding new design_01 css and basic page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Helmet } from 'react-helmet';
 import '../styles/index.css';
 
 function Index() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
   return (
     <main>
       <Helmet>
         <title>Pranay Yadav</title>
       </Helmet>
-      <div class = "bio-container">
-        <p class = "name">PRANAY YADAV</p>
-        <p>
-          Currently an early-stage VC at {' '}
-          <a
-            href="https://www.accel.com"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-          Accel
-          </a>{' '}
-          . I look at Enterprise Software, DevTools, OSS and API businesses.
-        </p>
-        <p>Beyond the usual, I help portfolio companies with product and GTM.</p>
-      </div>
+        <h1>Pranay Yadav</h1>
+        <p>Currently an early-stage VC at <a href="https://www.accel.com" rel="noopener noreferrer" target="_blank">Accel</a>. I look at Enterprise Software, DevTools, OSS and API businesses. Beyond the usual, I help portfolio companies with product and GTM.</p>
+        <nav>
+        <h2>Get in touch</h2>
+        <div><p><a href="mailto:pyadav@accel.com">Get in touch</a> with me over email here.</p></div>
+        </nav>
     </main>
   );
 }
